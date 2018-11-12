7ef69641632ea983ac47f7103dcd14c94e880ff8
yurickgomes <tecyurick@gmail.com>
2018-11-12 14:11:31 -0200

    Lint

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-const Routes =  (store) => (
+const Routes = (/*store*/) => (
   <Switch>
     <Route path="/" render={() => { }} exact />
   </Switch>
